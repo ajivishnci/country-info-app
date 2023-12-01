3042d92d81e190e5b4a73369d0dedf3fa67828df
Aji Vishwambharan <145560125+ajivishnci@users.noreply.github.com>
2023-12-01 11:58:19 +0000

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { TextField, Button, Container, Typography } from '@mui/material';
+import CountryInfo from './frontend/components/CountryInfo.js';
 
-function App() {
+const App = () => {
+  const [country, setCountry] = useState('');
+  const [countryInfo, setCountryInfo] = useState(null);
+
+  const fetchCountryInfo = async () => {
+    try {
+      const response = await fetch(`http://localhost:5000/country/${country}`);
+      const data = await response.json();
+      setCountryInfo(data);
+    } catch (error) {
+      console.error('Error fetching country information:', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="md" style={{ marginTop: '50px' }}>
+      <Typography variant="h3" align="center" gutterBottom>
+        Get Country Information
+      </Typography>
+      <TextField
+        label="Enter Country"
+        variant="outlined"
+        value={country}
+        onChange={(e) => setCountry(e.target.value)}
+        fullWidth
+        style={{ marginBottom: '20px' }}
+      />
+      <Button variant="contained" color="primary" onClick={fetchCountryInfo}>
+        Get Country Info
+      </Button>
+      {countryInfo && <CountryInfo countryInfo={countryInfo} />}
+    </Container>
   );
-}
+};
 
 export default App;
